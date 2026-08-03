ad8c53081dfb80d661a5d3fff53b27d4dc5d7d95
slifker <erenslifker@gmail.com>
2026-08-02 23:21:43 -0500

    Update mora-jai-code/pc_test/main_solutions.h
    updated solutions sourced from actual search based solver (puzzles imported directly from main_puzzles.h and fed into solver)

--- mora-jai-code/pc_test/main_solutions.h
+++ mora-jai-code/pc_test/main_solutions.h
@@ -1,97 +1,97 @@
 #ifndef MAIN_SOLUTIONS_H
 #define MAIN_SOLUTIONS_H
 
-#define s00 ""                                   // Grey Test
-#define s01 "01475"                              // Master Bedroom
-#define s02 "6656"                               // Solarium
-#define s03 "4244425424"                         // Clara Tomb
-#define s04 "0333"                               // Tunnel
-#define s05 "037433335"                          // Trading Post
-#define s06 "715202868714"                       // Closed Exhibit
-#define s07 "0557"                               // Lost and Found
-#define s08 "02121530"                           // Blue Prince Throne Room
-#define s09 "7556638777271"                      // Inner sanctum Orindia
-#define s10 "81166328577806730344"               // Inner sanctum Fenn Aries
-#define s11 "7578101762335"                      // Inner sanctum Arch Aries
-#define s12 "5113117220610204544566"             // Inner sanctum Eraja
-#define s13 "172206102002820"                    // Inner sanctum Corarica
-#define s14 "367444468"                          // Inner sanctum Mora Jai
-#define s15 "00313033474115"                     // Inner sanctum Verra
-#define s16 "058722505530"                       // Inner sanctum Nuance
-#define s17 "88387777"                           // The Unknown Box #1
-#define s18 "87753522111"                        // The Unknown Box #2
-#define s19 "633348825"                          // The Unknown Box #3
-#define s20 "8556644567711"                      // The Unknown Box #4
-#define s21 "22066622822117"                     // The Unknown Box #5
-#define s22 "7221116101181"                      // The Unknown Box #6
-#define s23 "7715101077100078"                   // The Unknown Box #7
-#define s24 "1017227477885166662"                // The Unknown Box #8
-#define s25 "5367861"                            // Underpass
-#define s26 "70511382201120"                     // Blueprints Conservatory (A1)
-#define s27 "2535557131"                         // Blueprints Maid's Chamber (A2)
-#define s28 "3745885320212635"                   // Blueprints Entrance Hall (A3)
-#define s29 "36743"                              // Blueprints Pantry (A4)
-#define s30 "677333770"                          // Blueprints Storeroom (A5)
-#define s31 "702786"                             // Blueprints Bedroom (B1)
-#define s32 "135543"                             // Blueprints Drawing Room (B2)
-#define s33 "155636"                             // Blueprints Gallery (B3)
-#define s34 "048484"                             // Blueprints Library (B4)
-#define s35 "1068448642"                         // Blueprints Cloister (B5)
-#define s36 "460513211206"                       // Blueprints Solarium (C1)
-#define s37 "55683456"                           // Blueprints Hallway (C2)
-#define s38 "67664"                              // Blueprints Dining Room (C3)
-#define s39 "388012277"                          // Blueprints Observatory (C4)
-#define s40 "315707727277757575771777"           // Blueprints East Wing Hall (C5)
-#define s41 "7111274586457025512600128865078146" // Blueprints Nook (D1)
-#define s42 "761436"                             // Blueprints Kitchen (D2)
-#define s43 "027778635"                          // Blueprints Corridor (D3)
-#define s44 "5658507777"                         // Blueprints Music Room (D4)
-#define s45 "8877708165"                         // Blueprints Lavatory (D5)
-#define s46 "015471068766828"                    // Blueprints Billiards Room (E1)
-#define s47 "44413243"                           // Blueprints Trophy Room (E2)
-#define s48 "5734735360"                         // Blueprints Foundation (E3)
-#define s49 "02304415007861447"                  // Blueprints Ballroom (E4)
-#define s50 "606447635377"                       // Blueprints Spare Room (E5)
-#define s51 "11077101120486"                     // Blueprints West Wing Hall (F1)
-#define s52 "4104538333203457337456"             // Blueprints Passageway (F2)
-#define s53 "76334446777"                        // Blueprints Dark Room (F3)
-#define s54 "104"                                // Blueprints Closet (F4)
-#define s55 "3460170762086"                      // Blueprints Parlor Room (F5)
-#define s56 "04712100"                           // Blueprints Gymnasium (G1)
-#define s57 "6301226770771344424"                // Blueprints Laundry Room (G2)
-#define s58 "77718846700185727"                  // Blueprints Guest Bedroom (G3)
-#define s59 "7577117876"                         // Blueprints Conference Room (G4)
-#define s60 "110011213113251"                    // Blueprints Den (G5)
-#define s61 "055032332"                          // Blueprints Foyer (H1)
-#define s62 "745453437710122"                    // Blueprints Pool (H2)
-#define s63 "853630835811"                       // Blueprints Servant's Quarters (H3)
-#define s64 "3412645748206"                      // Blueprints Pump Room (H4)
-#define s65 "770454635367727"                    // Blueprints Furnace (H5)
-#define s66 "6622657822061210106"                // Blueprints Archives (I1)
-#define s67 "604417560"                          // Blueprints Chapel (I2)
-#define s68 "11767141846778151764182"            // Blueprints Vestibule (I3)
-#define s69 "88880084800167"                     // Blueprints Coat Check (I4)
-#define s70 "033337433331338733"                 // Blueprints Aquarium (I5)
-#define s71 "1316511121631"                      // Blueprints 46th room (H5)
-#define s72 "0"                                  // Challenge Orange 1
-#define s73 "0"                                  // Challenge Black 1
-#define s74 "0"                                  // Challenge Grey 1
-#define s75 "0"                                  // Challenge Red
-#define s76 "0"                                  // Challenge Long 1
-#define s77 "0"                                  // Challenge Pink 1
-#define s78 "0"                                  // Challenge Pink 2
-#define s79 "0"                                  // Challenge Grey 2
-#define s80 "0"                                  // Challenge Green
-#define s81 "0"                                  // Challenge Misc 1
-#define s82 "0"                                  // Challenge Misc 2
-#define s83 "0"                                  // Challenge Black 2
-#define s84 "0"                                  // Challenge Blue
-#define s85 "0"                                  // Challenge Violet
-#define s86 "0"                                  // Challenge Misc 3
-#define s87 "0"                                  // Challenge Misc 4
-#define s88 "0"                                  // Challenge Misc 5
-#define s89 "0"                                  // Challenge Yellow
-#define s90 "0"                                  // Challenge Orange 2
-#define s91 "0"                                  // Challenge Longer
+#define s00 ""                                    // Grey Test
+#define s01 "87"                                  // Master Bedroom
+#define s02 "6566"                                // Solarium
+#define s03 "444113"                              // Clara Tomb
+#define s04 "0333"                                // Tunnel
+#define s05 "035737333"                           // Trading Post
+#define s06 "1274135276"                          // Closed Exhibit
+#define s07 "0557"                                // Lost and Found
+#define s08 "02121035"                            // Blue Prince Throne Room
+#define s09 "174216063818"                        // Inner sanctum Orindia
+#define s10 "8185125414"                          // Inner sanctum Fenn Aries
+#define s11 "357781476"                           // Inner sanctum Arch Aries
+#define s12 "5113117412702443"                    // Inner sanctum Eraja
+#define s13 "7711866722876"                       // Inner sanctum Corarica
+#define s14 "343434468"                           // Inner sanctum Mora Jai
+#define s15 "04103030374"                         // Inner sanctum Verra
+#define s16 "058722505530"                        // Inner sanctum Nuance
+#define s17 "3878767"                             // The Unknown Box #1
+#define s18 "5223310167"                          // The Unknown Box #2
+#define s19 "633324827"                           // The Unknown Box #3
+#define s20 "63133388767"                         // The Unknown Box #4
+#define s21 "22001666167"                         // The Unknown Box #5
+#define s22 "7020116001181"                       // The Unknown Box #6
+#define s23 "337711272221777"                     // The Unknown Box #7
+#define s24 "021260063880043818"                  // The Unknown Box #8
+#define s25 "487"                                 // Underpass
+#define s26 "7011136666"                          // Blueprints Conservatory (A1)
+#define s27 "0013173"                             // Blueprints Maid's Chamber (A2)
+#define s28 "374853041537483"                     // Blueprints Entrance Hall (A3)
+#define s29 "315"                                 // Blueprints Pantry (A4)
+#define s30 "63777775"                            // Blueprints Storeroom (A5)
+#define s31 "702768"                              // Blueprints Bedroom (B1)
+#define s32 "135543"                              // Blueprints Drawing Room (B2)
+#define s33 "1555636"                             // Blueprints Gallery (B3)
+#define s34 "048484"                              // Blueprints Library (B4)
+#define s35 "160147"                              // Blueprints Cloister (B5)
+#define s36 "0700550522"                          // Blueprints Solarium (C1)
+#define s37 "55634685"                            // Blueprints Hallway (C2)
+#define s38 "0364"                                // Blueprints Dining Room (C3)
+#define s39 "312227"                              // Blueprints Observatory (C4)
+#define s40 "0152552173"                          // Blueprints East Wing Hall (C5)
+#define s41 "71133866347213366323"                // Blueprints Nook (D1)
+#define s42 "46436"                               // Blueprints Kitchen (D2)
+#define s43 "027677583"                           // Blueprints Corridor (D3)
+#define s44 "555333"                              // Blueprints Music Room (D4)
+#define s45 "66607726220"                         // Blueprints Lavatory (D5)
+#define s46 "01546310147"                         // Blueprints Billiards Room (E1)
+#define s47 "44413243"                            // Blueprints Trophy Room (E2)
+#define s48 "5734735306"                          // Blueprints Foundation (E3)
+#define s49 "01701068731"                         // Blueprints Ballroom (E4)
+#define s50 "046776386"                           // Blueprints Spare Room (E5)
+#define s51 "1177107410"                          // Blueprints West Wing Hall (F1)
+#define s52 "410242416331"                        // Blueprints Passageway (F2)
+#define s53 "784"                                 // Blueprints Dark Room (F3)
+#define s54 "104"                                 // Blueprints Closet (F4)
+#define s55 "034638720"                           // Blueprints Parlor Room (F5)
+#define s56 "01034322"                            // Blueprints Gymnasium (G1)
+#define s57 "23007731073"                         // Blueprints Laundry Room (G2)
+#define s58 "7711237771478"                       // Blueprints Guest Bedroom (G3)
+#define s59 "3777117876"                          // Blueprints Conference Room (G4)
+#define s60 "110318505"                           // Blueprints Den (G5)
+#define s61 "015560"                              // Blueprints Foyer (H1)
+#define s62 "6743545006"                          // Blueprints Pool (H2)
+#define s63 "833487416431"                        // Blueprints Servant's Quarters (H3)
+#define s64 "341000763"                           // Blueprints Pump Room (H4)
+#define s65 "770454635367727"                     // Blueprints Furnace (H5)
+#define s66 "260001382021216018"                  // Blueprints Archives (I1)
+#define s67 "036417306"                           // Blueprints Chapel (I2)
+#define s68 "4671178415542"                       // Blueprints Vestibule (I3)
+#define s69 "666627463"                           // Blueprints Coat Check (I4)
+#define s70 "0336335183573"                       // Blueprints Aquarium (I5)
+#define s71 "1316511121631"                       // Blueprints 46th room (H5)
+#define s72 "3025574377114207718880566627786"                                                         // challenge_orange_1
+#define s73 "777757772777577577257772777542034217"                                                    // challenge_black_1
+#define s74 "111151351144444463444447777717777077477"                                                 // challenge_grey_1
+#define s75 "23437413117412201720018502104811205278360"                                               // challenge_red
+#define s76 "53850450038122011466722876115720011821217413176"                                         // challenge_long_1
+#define s77 "0085550050220301001015830"                                                               // challenge_pink_1
+#define s78 "2263015216003336667647482"                                                               // challenge_pink_2
+#define s79 "7740777854277674377574277637400377784577374077784577374037778542776"                     // challenge_grey_2
+#define s80 "01416438304111151538186754300635522"                                                     // challenge_green
+#define s81 "122678500322685003228500332228500322685003225006322850032285003"                         // challenge_misc_1
+#define s82 "2678558387714866285838638003333336658562276325232038201272887"                           // challenge_misc_2
+#define s83 "1266755386457343536845734356127387304313373564100167"                                    // challenge_black_2
+#define s84 "001785532045134351304513433513045134350167338336485480"                                  // challenge_blue
+#define s85 "014455532045134351304513435305451342233041553204543643536"                               // challenge_violet
+#define s86 "225555455574558857787643828876388586787388076384828578830176637"                         // challenge_misc_3
+#define s87 "013233465364553787465734353674558523566463537458534053244530147255"                      // challenge_misc_4
+#define s88 "6734882586741553864573435201234155343036512530864752513043538611120"                     // challenge_misc_5
+#define s89 "123346536674467867645734734087657343057857320674353345835"                               // challenge_yellow
+#define s90 "33753745431465687364635304513546755432403057857320674136585320"                          // challenge_orange_2
+#define s91 "266755386457343536845734354368457343561273306415715138664375153018645253777637866378"    // challenge_longer
 
 #endif // MAIN_SOLUTIONS_H
